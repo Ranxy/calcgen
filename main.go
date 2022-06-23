94f5287ef857a05d2c62217eb72ec5f7a598034a
Ranxy <3327004+Ranxy@users.noreply.github.com>
2022-06-23 15:08:59 +0800

    add code generate calc using plugin

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"plugin"
 	"strings"
 
 	"github.com/Ranxy/calcgen/node"
@@ -10,9 +13,12 @@ import (
 
 func main() {
 	query := Parse(strings.NewReader(os.Args[1]))
-	s := query.Expr.GenerateCode()
 
-	fmt.Println("S:", s)
+	code := generateCode(query)
+	fmt.Println("S:", code)
+
+	buildPlugin(code)
+	callPlugin()
 
 	env := node.Env{
 		"a": 2.1,
@@ -20,4 +26,32 @@ func main() {
 	value := query.Expr.Closures()(env)
 	fmt.Println(value)
 
+}
+
+func buildPlugin(code string) {
+	ioutil.WriteFile("/tmp/calc.go", []byte(code), 0644)
+	cmd := exec.Command("go", "build", "-buildmode=plugin", "-o", "/tmp/a.so", "/tmp/calc.go")
+	cmd.Run()
+}
+
+func callPlugin() {
+	p, err := plugin.Open("/tmp/a.so")
+	if err != nil {
+		panic(err)
+	}
+	sym, err := p.Lookup("Calc")
+	if err != nil {
+		panic(err)
+	}
+	sym.(func())()
+}
+
+func generateCode(q Query) string {
+	temp := `package main
+import "fmt"
+func Calc() {
+	res:=%s
+	fmt.Println(res)
+}`
+	return fmt.Sprintf(temp, q.Expr.GenerateCode())
 }
